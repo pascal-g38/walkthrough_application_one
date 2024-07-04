a7f4e01e2f71df02ecbb66e220442f4257c74fed
pascal-g38 <pascalgolomb@outlook.de>
2024-07-04 10:17:32 +0200

    Walkthrough Step 6

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], (Controller) => {
+    "sap/ui/core/mvc/Controller" ,
+    "sap/m/MessageToast"
+ ], (Controller, MessageToast) => {
     "use strict";
  
     return Controller.extend("ui5.walkthrough.controller.App", {
        onShowText() {
-          // show a native JavaScript alert
-          alert("Beispieltext oben");
+          MessageToast.show("Beispieltext unten");
        }
     });
  });
